85d931fd3228b31184416b4981b77d2bb1022b98
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 20:05:21 +0500

    Element and Task Schema is Made

---
+++ DB/Model/ElementModel.js
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from 'mongoose'
+
+const TaskSchema = new Schema({
+  ID: { type: String, required: true },
+  Text: { type: String, required: true },
+  ISDone: { type: Boolean, default: false },
+  CreatedAt: { type: Date, default: Date.now },
+})
+
+const ElementSchema = new Schema({
+  _id: { type: String },
+  Text: { type: String, required: true },
+  Tasks: [TaskSchema], // Array of TaskSchema
+  CreatedAt: { type: Date, default: Date.now },
+})
+
+const ElementModel = mongoose.model('Element', ElementSchema)
+
+export default ElementModel
