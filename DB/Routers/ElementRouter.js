0903c5d88a039bc8670620ade053f55604192aac
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 20:16:56 +0500

    added a get element route

--- DB/Routers/ElementRouter.js
+++ DB/Routers/ElementRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import AddElement from '../Controllers/Element/CreateElement.js'
 import { deleteElement } from '../Controllers/Element/DeleteElement.js'
+import { GetAllUserElements } from '../Controllers/Element/GetElementByUser.js'
 const ElementRouter = Router()
 ElementRouter.post('/MakeElement', AddElement)
-
+ElementRouter.get('/GetUserElements', GetAllUserElements)
 ElementRouter.delete('/DeleteElement', deleteElement)
 export default ElementRouter
