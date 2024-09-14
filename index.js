a6b2031ce6f46db45e6c7c8b77daa4a40c4080b6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 20:04:23 +0500

    Added 2 new routers for element and task

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import cors from 'cors'
 import { Port } from './Config.js'
 import AuthRouter from './DB/Routers/AuthRouter.js'
 import { DB_Connect } from './DB/DBConnect.js'
-
+import ElementRouter from './DB/Routers/ElementRouter.js'
+import TaskRouter from './DB/Routers/TaskRouter.js'
 const app = express()
-// Enable CORS for all origins
 const corsOptions = {
   origin: true, // Allow all origins https://notes-app-node-next-9x72.vercel.app/
   optionsSuccessStatus: 200, // For legacy browser support
@@ -13,6 +13,8 @@ const corsOptions = {
 app.use(express.json())
 app.use(cors(corsOptions))
 app.use('/api/Auth', AuthRouter)
+app.use('/api/Element', ElementRouter)
+app.use('/api/Task', TaskRouter)
 app.listen(Port, () => {
   console.log('RUNNING ON PORT 8000')
 })
